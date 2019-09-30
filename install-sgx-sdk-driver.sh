f5765a2164fd85900540fb05a28d18eb10940b32
clement.sciascia <clement.sciascia@gmail.com>
2019-09-30 08:28:23 +0000

    Scripts to (un)install SGX SDK & driver

---
+++ install-sgx-sdk-driver.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get install -y build-essential ocaml ocamlbuild automake autoconf libtool wget python libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev debhelper cmake
+
+BASE_DIR=/opt/intel
+
+[[ -d $BASE_DIR ]] || sudo mkdir -p $BASE_DIR && sudo chown sgx:sgx $BASE_DIR
+
+cd $BASE_DIR
+git clone https://github.com/intel/linux-sgx.git
+cd linux-sgx
+git checkout sgx_2.6
+./download_prebuilt.sh
+make -j 6
+make sdk_install_pkg -j 6
+make deb_pkg -j 6
+$BASE_DIR/linux-sgx/linux/installer/bin/sgx_linux_x64_sdk_2.6.100.51363.bin --prefix=$BASE_DIR/
+
+sudo dpkg -i $BASE_DIR/linux-sgx/linux/installer/deb/libsgx-urts_2.6.100.51363-bionic1_amd64.deb $BASE_DIR/linux-sgx/linux/installer/deb/libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb
+
+cd $BASE_DIR
+wget https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_driver_2.5.0_2605efa.bin
+chmod +x sgx_linux_x64_driver_2.5.0_2605efa.bin
+sudo ./sgx_linux_x64_driver_2.5.0_2605efa.bin
---
+++ uninstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR=/opt/intel
+
+[[ -d $BASE_DIR/libsgx-enclave-common/ ]] && sudo $BASE_DIR/libsgx-enclave-common/cleanup.sh
+if [[ -d $BASE_DIR/sgxdriver/ ]]; then
+       	sudo $BASE_DIR/sgxdriver/uninstall.sh
+	rm $BASE_DIR/sgx_linux_x64_driver_2.5.0_2605efa.bin
+fi
+sudo dpkg -P libsgx-urts libsgx-enclave-common
+[[ -d $BASE_DIR/sgxsdk/ ]] && sudo $BASE_DIR/sgxsdk/uninstall.sh
+[[ -d $BASE_DIR/linux-sgx/ ]] && sudo rm -rf $BASE_DIR/linux-sgx/
+
+if [[ -d $BASE_DIR/sgx-ra-sample/ ]]; then
+	sudo rm -rf $BASE_DIR/sgx-ra-sample/
+	sudo rm $BASE_DIR/Intel_SGX_Attestation_RootCA.pem
+fi
+
+if [ -z "$(ls -A $BASE_DIR)" ]; then
+	sudo rmdir $BASE_DIR
+fi
